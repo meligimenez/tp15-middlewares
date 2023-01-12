b5cd1dfbb27d28df0e5a73b3b58a85b3db628fe4
meligimenez <melinagimenez.dev@gmail.com>
2023-01-11 22:39:03 -0300

    estructure

---
+++ README.md
@@ -0,0 +1,3 @@
+# TP N°15 Middlewares
+
+>[Middlewares](https://drive.google.com/file/d/1r3pbnaFr8hynvqQllpyhKXW5TYaYx63U/view)
---
+++ src/routes/main.js
@@ -0,0 +1,7 @@
+const router = require('express').Router();
+const {index} = require('../controllers/mainController');
+
+router
+  .get('/', index)
+
+module.exports = router;
